1b7c30127212fcc55738dd44309142fb05c57e28
Jeremy Garey <jeremy@mirusresearch.com>
2024-02-04 20:21:22 -0600

    clean up slack plugin

--- sketchybar/plugins/slack.sh
+++ sketchybar/plugins/slack.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env sh
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Slack")
-SHOW_LABEL=false
 ICON_PADDING_RIGHT=12
 DRAWING=false
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     LABEL="${BASH_REMATCH[1]}"
 
     if [[ $LABEL == "" ]]; then
-        ICON_COLOR="0xffa6da95"
-        ICON_PADDING_RIGHT=12
-        SHOW_LABEL=false
         DRAWING=false
     elif [[ $LABEL == "•" ]]; then
-        ICON_COLOR="0xffeed49f"
         ICON_PADDING_RIGHT=4
-        SHOW_LABEL=true
         DRAWING=true
     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
-        ICON_COLOR="0xffed8796"
         ICON_PADDING_RIGHT=4
-        SHOW_LABEL=true
         DRAWING=true
     else
         exit 0
@@ -28,4 +20,4 @@ else
   exit 0
 fi
 
-sketchybar --set $NAME label="${LABEL}" label.drawing=$SHOW_LABEL icon.color=${ICON_COLOR} icon.padding_right=${ICON_PADDING_RIGHT} drawing=$DRAWING
+sketchybar --set $NAME label="${LABEL}" icon.padding_right=${ICON_PADDING_RIGHT} drawing=$DRAWING
