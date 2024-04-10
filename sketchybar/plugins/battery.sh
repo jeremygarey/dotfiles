aa91b363731e0988ebcfa6d016c2f6f736f1f05c
Jeremy Garey <jeremy@mirusresearch.com>
2024-04-10 09:27:14 -0500

    more small improvements

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -2,25 +2,28 @@
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
+AC_BUT_NOT_CHARGING=$(sleep 1; pmset -g batt | grep 'AC attached; not charging present: true')
 
 if [ $PERCENTAGE = "" ]; then
   exit 0
 fi
 
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="􀛨"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="􀺸"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="􀺶"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="􀛩"
   ;;
-  *) ICON=""
+  *) ICON="􀛪"
 esac
 
-if [[ $CHARGING != "" ]]; then
-  ICON=""
+if [[ $AC_BUT_NOT_CHARGING != "" ]]; then
+  ICON="􀋨"
+elif [[ $CHARGING != "" ]]; then
+  ICON="􀋦"
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
