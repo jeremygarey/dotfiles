d856f5f7261c7dfdd0ea0c803957495880524a86
Jeremy Garey <jeremy@mirusresearch.com>
2025-01-01 13:00:05 -0600

    audio device updates

--- sketchybar/plugins/audio_output.sh
+++ sketchybar/plugins/audio_output.sh
@@ -2,13 +2,13 @@
 
 OUTPUT_DEVICE=$(SwitchAudioSource -c)
 
-if [ "$OUTPUT_DEVICE" = "Jeremy’s AirPods Pro" ]; then
+if [ "$OUTPUT_DEVICE" = "Jeremy’s AirPods Pro #2" ]; then
     sketchybar --set $NAME drawing=on icon=􀪷
 elif [ "$OUTPUT_DEVICE" = "POD Go" ]; then
     sketchybar --set $NAME drawing=on icon=􀟑
 elif [ "$OUTPUT_DEVICE" = "Fosi Audio" ]; then
     sketchybar --set $NAME drawing=on icon=􀝏
-elif [ "$OUTPUT_DEVICE" = "MDR-1000X" ]; then
+elif [ "$OUTPUT_DEVICE" = "WH-1000XM5" ]; then
     sketchybar --set $NAME drawing=on icon=􀺹
 else
     sketchybar --set $NAME drawing=off
