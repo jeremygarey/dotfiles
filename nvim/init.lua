eb7db7387c528a28e69882e4278d547c7e52b947
Jeremy Garey <jeremy@mirusresearch.com>
2025-11-04 10:02:07 -0600

    enable syntax highlighting for handlebars files. commit lock file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- enable syntax highlighting for handlebars files
+vim.cmd("autocmd BufRead,BufNewFile *.hbs set filetype=html")
